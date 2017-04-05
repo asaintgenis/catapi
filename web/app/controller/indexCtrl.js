de134ad6aff1fc2c78dcf1f4bb160118b53e6aab
Arthur Saint-Genis <a.saintgenis@gmail.com>
2017-04-05 08:50:26 +0200

    module register and inject http, close #1

--- web/app/controller/indexCtrl.js
+++ web/app/controller/indexCtrl.js
@@ -7,15 +7,20 @@
  * # IndexCtrl
  * Controller of the index page of catapi
  */
-angular.module('catapi')
-  .controller('IndexCtrl', function ($scope) {
+
+angular.module('catapi',[])
+  .controller('IndexCtrl', function($scope,$http) {
         $scope.catImage = "";
+        console.log("controller is loaded");
 
         $scope.getCat = function(){
           console.log("get incoming....");
           $http.get('http://localhost:8080/cats')
           .then(function(response) {
+              console.log(response.data);
               $scope.catImage = response.data.pic;
+              console.log($scope.catImage);
           });
         };
+
   });
